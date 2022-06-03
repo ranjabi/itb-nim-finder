a62828d6b220f27d02ff7234335faf2fb483f832
ranjabi <fikriranjabi02@gmail.com>
2022-06-03 14:42:59 +0700

    add search by nim

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
+/*
+ * using data_13_21.json
+ */
+
+const data = require("./json/data_13_21.json");
+const kodeJurusan = require('./json/kode_jurusan.json');
+
+const getKodeJurusan = (nim) => {
+}
+
+
+// /////////////////////////////////////////////////
 function App() {
+  const [query, setQuery] = useState("");
+  // const [result, setResult] = useState([]);
+
+  // useEffect(() => {
+  //   if (query.length > 0) {
+  //     setResult(filtered);
+  //   } else {
+  //     setResult([]);
+  //   }
+  // }, [query]);
+
+  
+
+  const searchQueryHandler = (event) => {
+    setQuery(event.target.value);
+    console.log(query)
+    // const byNimMatch = new RegExp(query)
+    // setResult(data.match(byNimMatch));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Find Someone!</h1>
+      <input
+        type="text"
+        name="query"
+        id=""
+        placeholder="format"
+        onChange={(event) => searchQueryHandler(event)}
+      />
+      {data
+        .filter((val, idx) => idx < 10)
+        .filter((val) => {
+          const byNimMatch = new RegExp(query);
+          const byJurusanMatch = {
+            
+          }
+
+          const nimFakultas = val[1];
+          const nimJurusan = val[2];
+          if (byNimMatch.test(nimFakultas) || byNimMatch.test(nimJurusan)) {
+            return val;
+          }
+
+        })
+        .map((val, key) => {
+          return (
+            <div key={key}>
+              <p>
+                {val[0]} {val[1]} {val[2]}
+              </p>
+            </div>
+          );
+        })}
     </div>
   );
 }
