aa9196e7151e5af607e357d4c0c3230a97147555
ranjabi <fikriranjabi02@gmail.com>
2022-06-04 21:05:38 +0700

    add button

--- src/App.js
+++ src/App.js
@@ -52,15 +52,122 @@ const getKodeFakultas = (namaFakultas) => {
 
 function App() {
   const [query, setQuery] = useState("");
+  const [res, setRes] = useState([]);
 
   const searchQueryHandler = (event) => {
     setQuery(event.target.value);
     console.log(query)
   };
 
+  const result = (query) => {
+    setRes(data.filter(function(val) {
+      if (this.count < 10) {
+        const nama = val[0];
+        const nimFakultas = val[1];
+    
+        let nimJurusan = 9999;
+        if (val.length === 3) {
+          nimJurusan = val[2];
+        }
+    
+        const plainQueryAsPattern = new RegExp(query);
+        const searchMethod = 0;
+        
+        const byNim = new RegExp(/\d{3,8}/);
+        const byName = new RegExp(/([a-zA-Z]{3,}\s?)+/);
+    
+        const byNimName = new RegExp(/\d{3,8}\s([a-zA-Z]{3,}\s?)+/);
+        const byNameNim = new RegExp(/([a-zA-Z]{3,}\s?)+\d{3,8}/);
+    
+        const byMajorname = new RegExp(/([a-zA-Z]{2,}\s?)+/);
+    
+        if (byNim.test(query)) {
+          // query nim
+          if (plainQueryAsPattern.test(nimFakultas) || plainQueryAsPattern.test(nimJurusan)) {
+            // kembalikan data yang memiliki nim yang sama dengan query
+            console.log("byNim");
+            this.count++;
+            return val;
+          }
+        }
+        if (byName.test(query)) {
+          // query nama
+          if (plainQueryAsPattern.test(nama.toLowerCase())) {
+            console.log("byName");
+            this.count++;
+            return val;
+          }
+        }
+        if (byNimName.test(query)) {
+          // query <nim> <nama>
+          const splittedQuery = query.split(" ");
+          const nimPattern = new RegExp(splittedQuery[0]);
+          const namePattern = new RegExp(splittedQuery.slice(1).join(" "));
+    
+          if ((nimPattern.test(nimFakultas) || nimPattern.test(nimJurusan)) && namePattern.test(nama.toLowerCase())) {
+            console.log("byNimName");
+            this.count++;
+            return val;
+          }
+        }
+        if (byNameNim.test(query)) {
+          // query <nama> <nim>
+          const splittedQuery = query.split(" ");
+          const nimPattern = new RegExp(splittedQuery[splittedQuery.length - 1]);
+          const namePattern = new RegExp(splittedQuery.slice(0,-1).join(" "));
+    
+          if ((nimPattern.test(nimFakultas) || nimPattern.test(nimJurusan)) && namePattern.test(nama.toLowerCase())) {
+            console.log("byNameNim");
+            this.count++;
+            return val;
+          }
+        }
+    
+        // TODO: bikin input jurusan ex. informatika matematika dari list_fakultas.json and list_jurusan.json
+    
+        if (byMajorname.test(query)) {
+          // input jurusan. ex: IF MA
+          const splittedQuery = query.split(" ");
+          const majorQuery = splittedQuery[splittedQuery.length - 1];
+          const namePattern = new RegExp(splittedQuery.slice(0, -1).join(" "));
+    
+          if (getKodeFakultas(majorQuery) !== -1 || getKodeJurusan(majorQuery) !== -1) {
+            const nimPattern1 = new RegExp(getKodeJurusan(majorQuery));
+            const nimPattern2 = new RegExp(getKodeFakultas(majorQuery));
+            if (namePattern.test(nama.toLowerCase()) && nimPattern2.test(nimFakultas) || nimPattern1.test(nimJurusan)) {
+              console.log("byMajorName");
+              this.count++;
+              return val;
+            }
+          }
+        }
+      }
+    }, {count: 0}))
+    
+  } 
+
+  const showResult = () => {
+    
+  }
+
+  const resultList = () => {
+    result(query)===undefined? void(0) : result(query);
+  } 
+    
+
+  
+  // .filter((val, idx) => idx < 10)
+  // .map((val, key) => {
+  //   return (
+      
+  //   );
+  // })
+
   return (
     <div>
       <h1>Find Someone!</h1>
+      {/* <form> */}
+
       <input
         type="text"
         name="query"
@@ -68,108 +175,30 @@ function App() {
         placeholder="format"
         onChange={(event) => searchQueryHandler(event)}
       />
-      {data
-        .filter((val) => {
-          const nama = val[0];
-          const nimFakultas = val[1];
-
-          let nimJurusan = 9999;
-          if (val.length === 3) {
-            nimJurusan = val[2];
-          }
-
-          const plainQueryAsPattern = new RegExp(query);
-          const searchMethod = 0;
-          
-          const byNim = new RegExp(/\d{3,8}/);
-          const byName = new RegExp(/([a-zA-Z]{3,}\s?)+/);
-
-          const byNimName = new RegExp(/\d{3,8}\s([a-zA-Z]{3,}\s?)+/);
-          const byNameNim = new RegExp(/([a-zA-Z]{3,}\s?)+\d{3,8}/);
-
-          const byMajorname = new RegExp(/([a-zA-Z]{2,}\s?)+/);
-
-          if (byNim.test(query)) {
-            // query nim
-            if (plainQueryAsPattern.test(nimFakultas) || plainQueryAsPattern.test(nimJurusan)) {
-              // kembalikan data yang memiliki nim yang sama dengan query
-              console.log("byNim");
-              return val;
-            }
-          }
-          if (byName.test(query)) {
-            // query nama
-            if (plainQueryAsPattern.test(nama.toLowerCase())) {
-              console.log("byName");
-              return val;
-            }
-          }
-          if (byNimName.test(query)) {
-            // query <nim> <nama>
-            const splittedQuery = query.split(" ");
-            const nimPattern = new RegExp(splittedQuery[0]);
-            const namePattern = new RegExp(splittedQuery.slice(1).join(" "));
-
-            if ((nimPattern.test(nimFakultas) || nimPattern.test(nimJurusan)) && namePattern.test(nama.toLowerCase())) {
-              console.log("byNimName");
-              return val;
-            }
-          }
-          if (byNameNim.test(query)) {
-            // query <nama> <nim>
-            const splittedQuery = query.split(" ");
-            const nimPattern = new RegExp(splittedQuery[splittedQuery.length - 1]);
-            const namePattern = new RegExp(splittedQuery.slice(0,-1).join(" "));
-
-            if ((nimPattern.test(nimFakultas) || nimPattern.test(nimJurusan)) && namePattern.test(nama.toLowerCase())) {
-              console.log("byNameNim");
-              return val;
-            }
-          }
-
-          // TODO: bikin input jurusan ex. informatika matematika dari list_fakultas.json and list_jurusan.json
-          // TODO: pake mongodb biar bisa limit result
-
-          if (byMajorname.test(query)) {
-            // input jurusan. ex: IF MA
-            const splittedQuery = query.split(" ");
-            const majorQuery = splittedQuery[splittedQuery.length - 1];
-            const namePattern = new RegExp(splittedQuery.slice(0, -1).join(" "));
-
-            if (getKodeFakultas(majorQuery) !== -1 || getKodeJurusan(majorQuery) !== -1) {
-              const nimPattern1 = new RegExp(getKodeJurusan(majorQuery));
-              const nimPattern2 = new RegExp(getKodeFakultas(majorQuery));
-              if (namePattern.test(nama.toLowerCase()) && nimPattern2.test(nimFakultas) || nimPattern1.test(nimJurusan)) {
-                console.log("byMajorName");
-                return val;
-              }
-            }
-
-          }
-
-        })
-        .filter((val, idx) => idx < 10)
-        .map((val, key) => {
-          return (
-            <div key={key}>
-              <p>
-                {val[0]} 
-                {" "}
-                {val[1]} 
-                {" "}
-                { 
-                  (val.length === 3) &&
-                  val[2]
-                } 
-                {" "}
-                {
-                  (val.length === 3) ? getJurusan(val[2].toString().slice(0,3)) : getFakultas(val[1].toString().slice(0,3)) 
-
-                }
-              </p>
-            </div>
-          );
-        })}
+      <button type="submit" onClick={ resultList }>Search</button>
+      {/* </form> */}
+      {res.map((val, key) => {
+      return (
+  
+    <div key={key}>
+    <p>
+      {val[0]} 
+      {" "}
+      {val[1]} 
+      {" "}
+      { 
+        (val.length === 3) &&
+        val[2]
+      } 
+      {" "}
+      {
+        (val.length === 3) ? getJurusan(val[2].toString().slice(0,3)) : getFakultas(val[1].toString().slice(0,3)) 
+  
+      }
+    </p>
+  </div>
+      )
+    })}
     </div>
   );
 }
